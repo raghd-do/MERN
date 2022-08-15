e8f3f779c36007e157e0a96dfa16dbb3371e2127
Raghad AlJabr <alraghad188@gmail.com>
2022-08-15 07:54:37 +0300

    Assignment 5

---
+++ JS/Ninja.js
@@ -0,0 +1,23 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(`May name is ${this.name}`);
+    }
+    showStats() {
+        console.log(
+            `Name: ${this.name}\nStrength: ${this.strength}\nSpeed: ${this.speed}\nHealth: ${this.health}`);
+    }
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
