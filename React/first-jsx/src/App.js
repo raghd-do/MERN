57df69fb8b748b00c4e20d6f70adc5f949075d24
Raghad AlJabr <alraghad188@gmail.com>
2022-08-16 16:13:22 +0300

    First React With JSX

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Lern React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the birds</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
